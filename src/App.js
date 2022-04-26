3297cf3c1c216007e9232f5e28defb32f3be4893
ChrisBoisjoli <christopherboisjoli@gmail.com>
2022-04-26 10:23:40 -0500

    main UI function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+// import { HashRouter as Router, Route } from "react-router-dom";
+import React from 'react';
 import './App.css';
+import Title from "./components/Title";
+import Wrapper from "./components/Wrapper";
+import PortfolioCard from './components/ProjectCard';
+import friends from "./friends.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Projects</Title>
+      <PortfolioCard
+      name={friends[1].name}
+      image={friends[1].image }
+      occupation={friends[1].occupation}
+      loction={friends[1].location}/>
+      <PortfolioCard
+      name={friends[2].name}
+      image={friends[2].image }
+      occupation={friends[2].occupation}
+      loction={friends[2].location}/>
+    
+    </Wrapper>
   );
 }
 
