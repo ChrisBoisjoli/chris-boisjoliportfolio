ff12a6fe2a7e07df788b11f606e882193eceda75
ChrisBoisjoli <christopherboisjoli@gmail.com>
2022-04-26 14:57:10 -0500

    working router links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-// import { HashRouter as Link, Router, Route } from "react-router-dom";
 import React from 'react';
+import { BrowserRouter as  Router, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Title from "./components/Title";
-import Wrapper from "./components/Wrapper";
+// import Wrapper from "./components/Wrapper";
 import PortfolioCard from './components/ProjectCard';
 import projects from "./projects.json";
+import NavTabs from "./components/NavTabs";
+// import Home from "./components/pages/Home";
+// import About from "./components/pages/About";
+// import Blog from "./components/pages/Blog";
+// import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <Wrapper>
+    <Router>
+    <div>
+
+    {/* <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} /> */}
       <Header/>
+      <NavTabs/>
       <Title>Projects</Title>
       <PortfolioCard
       name={projects[0].name}
@@ -20,8 +33,8 @@ function App() {
       github={projects[0].github}
       description={projects[0].description}/>
       <Footer/>
-    </Wrapper>
-    
+    </div>
+    </Router>
   );
 }
 
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function PortfolioCard(props) {
-  return (
+    return (
     <div className="card">
       <div className="img-container">
         <img alt={props.name} src={props.image} />
